650dd35593e61fd2bbf5ca4c7767654ecfcb02bc
maggbradley <166860075+maggbradley@users.noreply.github.com>
2024-08-29 23:09:03 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+test("Heading renders successfully", () => {
+  render(<BookingForm/>)
+
+  const element = screen.getByText(/Number of Guests/)
+  expect(element).toBeIntheDocument();
 });
